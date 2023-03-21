5de789410a33d51ea4cb94cded29e7cff2e3eaf0
POM-MOUYLANG <126035070+POMMOUYLANG@users.noreply.github.com>
2023-03-21 18:56:13 +0700

    Update Ex01.java

--- Exercise/Ex01.java
+++ Exercise/Ex01.java
@@ -2,6 +2,7 @@ package OOP_to_JAVA.Exercise;
 
 public class Ex01 {
     public static void main(String[] args) {
-        System.out.println("Hello Mouylang");
+        System.out.println("Hello Everyone");
+        System.out.print("Hi %s!","Mouylang");
     }
 }
